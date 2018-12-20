8f320279c5723461bf38b202f8fe167496c119c9
黒木保 <john@Johns-MacBook-Pro.local>
2018-12-20 12:12:25 +0900

    ちょっと変えた

--- test.java
+++ test.java
@@ -5,24 +5,21 @@ public class test{
 }
 class mineTable{
 	mineTable(int verticle, int width, int[] ... cells){
+		// cell[0]は周りに何個置くか
+		// 0 ~ 10
+		// 10は指定なし
+		
+		// cell[1]は置かれている色
+		// 初期値は0(無色)
+		// 0: 無色
+		// 1: 赤色
+		// 2: 緑色
+		// 3: 青色
 		int[][][] table = int[verticle][][]
 		for(int i = 0; i < verticle; i++){
 			table[i] = int[width][]
 			for(int j = 0; j < width; j++)
-				table[i][j] = cells[]
-		}
-		for(int[] cell: cells){
-			// restrictionは周りに何個置くか
-			// 0 ~ 10
-			// 10は指定なし
-			int restriction = cell[0]
-			// colorは置かれている色
-			// 初期値は0(無色)
-			// 0: 無色
-			// 1: 赤色
-			// 2: 緑色
-			// 3: 青色
-			int color = cell[1]
+				table[i][j] = [cells[i * width + j][0], cells[i * width + j][1]
 		}
 	}
 	display(){
